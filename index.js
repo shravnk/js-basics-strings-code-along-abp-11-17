6bf6f1f621fc2d44ced6caf30343350bfff9dc57
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-10 22:42:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const currentUser = 'Grace Hopper';
- 
+
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
